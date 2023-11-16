bbc02f8f2a399c8b7418aa36f67697884e54983a
InterstellarJ2049 <yijie-zhang@uiowa.edu>
2023-11-16 16:25:49 -0600

    add app testing code and try in chatgpt api

---
+++ utils/chatgpt_api.py
@@ -0,0 +1,60 @@
+import base64
+import requests
+
+# Function to encode the image
+def encode_image(image_path):
+  with open(image_path, "rb") as image_file:
+    return base64.b64encode(image_file.read()).decode('utf-8')
+
+# # Path to your image
+# image_path = "data/image/Seminar_Nov16.jpg"
+
+# # Getting the base64 string
+# base64_image = encode_image(image_path)
+
+# headers = {
+#   "Content-Type": "application/json",
+#   "Authorization": f"Bearer {api_key}"
+# }
+
+# payload = {
+#   "model": "gpt-4-vision-preview",
+#   "messages": [
+#     {
+#       "role": "user",
+#       "content": [
+#         {
+#           "type": "text",
+#           "text": "What’s in this image?"
+#         },
+#         {
+#           "type": "image_url",
+#           "image_url": {
+#             "url": f"data:image/jpeg;base64,{base64_image}"
+#           }
+#         }
+#       ]
+#     }
+#   ],
+#   "max_tokens": 300
+# }
+
+# response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+
+# print(response.json())
+
+# # Example usage of the function
+# image_path = 'path/to/your/image.jpg'  # Replace with the path to your image
+# prompt = "Describe the image."  # Replace with your prompt
+# response = analyze_image(image_path, prompt)
+# print(response)
+
+
+# def translate_text(text, target_language):
+#     # code to translate text to target language
+#     pass
+
+# def describe_image(image_path):
+#     # code to describe image using GPT
+#     pass
+
