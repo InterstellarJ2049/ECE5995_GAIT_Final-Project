3c29465f805dcdc140f2be7a76ff9025bec9ca18
tommywang51 <43577796+tommywang51@users.noreply.github.com>
2023-11-29 23:06:48 -0600

    Update on translation
    Added auto-detected language type from user's input and translate the foreign language to user's language.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify, render_template
 from utils.whisper_api import transcribe_audio
-#from utils.chatgpt_api import translate_text, describe_image
+from utils.chatgpt_api import detect_language, translate_text
 #from utils.tts_api import text_to_speech
 import os
 
@@ -16,21 +16,44 @@ def transcribe():
     filename = audio_file.filename
     if not filename.endswith('.wav'):  # Ensure it has the .wav extension
         filename += '.wav'
-    filename = os.path.join('./', filename)
+    filename = os.path.join('./audio_storage', filename)
     audio_file.save(filename)
 
     transcription = transcribe_audio(filename)
 
     os.remove(filename)
     return jsonify({'transcription': transcription})
-'''
+
+@app.route('/detect_language', methods=['POST'])
+def detect_language_route():
+    audio_file = request.files['audio']
+    filename = audio_file.filename
+    if not filename.endswith('.wav'):  # Ensure it has the .wav extension
+        filename += '.wav'
+    filepath = os.path.join('./audio_storage', filename)
+    audio_file.save(filepath)
+
+    # Transcribe the audio to text
+    transcription = transcribe_audio(filepath)
+
+    # Clean up: remove the audio file after transcription
+    os.remove(filepath)
+
+    # Detect the language of the transcribed text
+    detected_language = detect_language(transcription)
+    return jsonify({'detected_language': detected_language})
+
+
 @app.route('/translate', methods=['POST'])
 def translate():
-    text = request.json['text']
-    target_language = request.json['target_language']
-    translated_text = translate_text(text, target_language)
+    data = request.json
+    original_text = data.get('text')
+    target_language = data.get('target_language')
+
+    translated_text = translate_text(original_text, target_language)
     return jsonify({'translated_text': translated_text})
 
+'''
 @app.route('/tts', methods=['POST'])
 def tts():
     text = request.json['text']
--- static/js/script.js
+++ static/js/script.js
@@ -57,7 +57,88 @@ function sendAudioToServer(blob) {
         return response.json();
     }).then(data => {
         // Display the transcription
+        console.log( data.transcription );
         document.getElementById('transcriptionOutput').value = data.transcription;
     });
 }
 
+
+let isLanguageRecording = false;
+let languageMediaRecorder;
+let languageAudioChunks = [];
+
+document.getElementById('toggleLanguageRecordButton').addEventListener('click', () => {
+    if (!isLanguageRecording) {
+        startLanguageRecording();
+    } else {
+        stopLanguageRecording();
+    }
+});
+
+function startLanguageRecording() {
+    navigator.mediaDevices.getUserMedia({ audio: true })
+        .then(stream => {
+            languageMediaRecorder = new MediaRecorder(stream);
+            languageMediaRecorder.start();
+
+            languageAudioChunks = [];
+            languageMediaRecorder.addEventListener("dataavailable", event => {
+                languageAudioChunks.push(event.data);
+            });
+
+            isLanguageRecording = true;
+            document.getElementById('toggleLanguageRecordButton').textContent = 'Stop Recording';
+        });
+}
+
+function stopLanguageRecording() {
+    languageMediaRecorder.stop();
+    languageMediaRecorder.addEventListener("stop", () => {
+        const audioBlob = new Blob(languageAudioChunks, { 'type' : 'audio/wav' });
+        const audioUrl = URL.createObjectURL(audioBlob);
+
+        const audioPlayer = document.getElementById('languageAudioPlayback');
+        audioPlayer.src = audioUrl;
+        audioPlayer.style.display = 'block';
+
+        sendLanguageAudioToServer(audioBlob);
+
+        isLanguageRecording = false;
+        document.getElementById('toggleLanguageRecordButton').textContent = 'Start Recording';
+    });
+}
+
+function sendLanguageAudioToServer(blob) {
+    const formData = new FormData();
+    formData.append("audio", blob);
+
+    fetch('/detect_language', {
+        method: 'POST',
+        body: formData
+    }).then(response => response.json())
+    .then(data => {
+        document.getElementById('detectedLanguageOutput').value = data.detected_language;
+        // Call translateText() after obtaining the detected language
+        translateText();
+    })
+    .catch(error => console.error('Error:', error));
+}
+
+
+function translateText() {
+    const originalText = document.getElementById('transcriptionOutput').value;
+    const detectedLanguage = document.getElementById('detectedLanguageOutput').value;
+
+    fetch('/translate', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ text: originalText, target_language: detectedLanguage })
+    })
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById('translationOutput').value = data.translated_text;
+    })
+    .catch(error => console.error('Error:', error));
+}
